9d88b1902ec8876439e6b9af34db662e4a801d4c
pitamberrana <pitamber11rana@gmail.com>
2022-06-01 22:46:26 +0545

    solved

--- pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
@@ -1,1 +1,4 @@
 // YOUR CODE BELOW
+let myFirstName = "Pitamber";
+let myFavoriteNum = 11;
+let havingFun = true;
--- pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
@@ -1,3 +1,6 @@
 let happyNum = 1000;
 
 // YOUR CODE BELOW
+happyNum /= 10;
+happyNum -= 94;
+happyNum--;
