b39e6daa5ecf681e42ae0cb83af29b82b6a5de34
pitamberrana <pitamber11rana@gmail.com>
2022-06-20 11:26:12 +0545

    taco cache prblm

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function cacheSavings(callback) {
+  let cache = {};
+  function newFunction(arg) {
+    if (cache[arg]) {
+      return cache[arg];
+    } else {
+      let result = callback(arg);
+      cache[arg] = result;
+      return result;
+    }
+  }
+  return newFunction;
+}
